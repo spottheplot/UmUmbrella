b1a686d937cec029c113a991367d88ec9e26ecfd
Juan Herrero Macias <jn.herrerom@gmail.com>
2014-12-07 03:56:51 +0000

    Implemented InputHandler for Player object

--- core/src/com/umumbrella/framework/InputHandler.java
+++ core/src/com/umumbrella/framework/InputHandler.java
@@ -1,26 +1,52 @@
 package com.umumbrella.framework;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 import com.umumbrella.entities.Player;
 import com.umumbrella.entities.Umbrella;
 
 public class InputHandler implements InputProcessor {
 	Player player;
+	Umbrella umbrella;
 
     public InputHandler(Player player, Umbrella umbrella) {
         this.player = player;
+        this.umbrella = umbrella;
     }
-	
+
 	@Override
 	public boolean keyDown(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
+		// Player
+		switch (keycode) {
+		case (Input.Keys.W):
+		case (Input.Keys.LEFT):
+			player.setxVelocity(-Player.gethSpeed());
+			break;
+		case (Input.Keys.D):
+		case (Input.Keys.RIGHT):
+			player.setxVelocity(Player.gethSpeed());
+			break;
+		default:
+			break;
+		}
+		// TODO Add umbrella
+		return true;
+
 	}
 
 	@Override
 	public boolean keyUp(int keycode) {
-		// TODO Auto-generated method stub
-		return false;
+		// Player
+		switch (keycode) {
+		case (Input.Keys.W):
+		case (Input.Keys.LEFT):
+		case (Input.Keys.D):
+		case (Input.Keys.RIGHT):
+			player.setxVelocity(0);
+			break;
+		}
+		// TODO Add umbrella
+		return true;
 	}
 
 	@Override
@@ -58,5 +84,5 @@ public class InputHandler implements InputProcessor {
 		// TODO Auto-generated method stub
 		return false;
 	}
-	
+
 }
