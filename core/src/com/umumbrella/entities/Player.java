d7340379c0a6a5e41b9bfaa1eab34b781829df20
Juan Herrero Macias <jn.herrerom@gmail.com>
2014-12-07 03:55:58 +0000

    Added player class

--- core/src/com/umumbrella/entities/Player.java
+++ core/src/com/umumbrella/entities/Player.java
@@ -1,5 +1,72 @@
 package com.umumbrella.entities;
 
-public class Player {
+import com.badlogic.gdx.Gdx;
+import com.umumbrella.game.MainGame;
 
+public class Player {
+	private float xPosition, yPosition, xVelocity, yVelocity;
+	private int playerWidth, playerHeight;
+	private boolean jumping;
+	private static int hSpeed = 60;
+
+	public Player() {
+		xPosition = MainGame.WIDTH / 2;
+		//TODO: Determine ground position from background
+		yPosition = MainGame.HEIGHT * 5 / 2;
+		jumping = false;
+	}
+
+	public void update() {
+		//Constants velocity for now, implement acceleration or change of direction??
+		xPosition += xVelocity * Gdx.graphics.getDeltaTime();
+		yPosition += yVelocity * Gdx.graphics.getDeltaTime();
+	}
+
+	public void setxVelocity(float xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+
+	public void setyVelocity(float yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+
+	public void setJumping(boolean jumping) {
+		this.jumping = jumping;
+	}
+
+	public void setxPosition(float xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public void setyPosition(float yPosition) {
+		this.yPosition = yPosition;
+	}
+
+	public int getPlayerHeight() {
+		return playerHeight;
+	}
+
+	public int getPlayerWidth() {
+		return playerWidth;
+	}
+
+	public float getxPosition() {
+		return xPosition;
+	}
+
+	public float getxVelocity() {
+		return xVelocity;
+	}
+
+	public float getyPosition() {
+		return yPosition;
+	}
+
+	public float getyVelocity() {
+		return yVelocity;
+	}
+
+	public static int gethSpeed() {
+		return hSpeed;
+	}
 }
