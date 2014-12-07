3857ea23aee9477fa731dc42630475506177c22c
Jabawaka <oldworldremains@gmail.com>
2014-12-07 01:26:01 +0000

    All classes commit

--- core/src/com/umumbrella/entities/Bee.java
+++ core/src/com/umumbrella/entities/Bee.java
@@ -6,12 +6,13 @@ import umumbrella.TextureManager;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
 
 public class Bee {
 	
-	private float xPosition, yPosition, xVelocity, yVelocity;
-	private Texture beeTexture;
+	private float xPosition, yPosition, xVelocity, yVelocity, angle;
+	private TextureRegion beeTexture;
 	private int beeWidth, beeHeight;
 	private boolean bounced;
 	
@@ -20,38 +21,47 @@ public class Bee {
 		//Depending on the chance (0 to 1) specified as input the bee starts head first or sting first
 		if (MathUtils.random() < chance) {
 			bounced = false;
-			beeTexture = TextureManager.NORMALBEE;
+			
+			beeWidth = TextureManager.NORMALBEE.getWidth();
+			beeHeight = TextureManager.NORMALBEE.getHeight();
+			beeTexture = new TextureRegion(TextureManager.NORMALBEE, beeWidth, beeHeight);
 		}
 		else {
 			bounced = true;
-			beeTexture = TextureManager.BOUNCEDBEE;
-		}
+			
+			beeWidth = TextureManager.BOUNCEDBEE.getWidth();
+			beeHeight = TextureManager.BOUNCEDBEE.getHeight();
+			beeTexture = new TextureRegion(TextureManager.BOUNCEDBEE, beeWidth, beeHeight);
+		}		
 		
-		beeWidth = beeTexture.getWidth();
-		beeHeight = beeTexture.getHeight();
-		
-		//Initialize position in the upper part of the screen
+		//Initialize position out of the screen over the top
 		
 		xPosition = MathUtils.random(0, MainGame.WIDTH);
 		yPosition = MathUtils.random(MainGame.HEIGHT, 2 * MainGame.HEIGHT);
 		
 		xVelocity = 0; //For now equal to 0, check for possible range in random
 		yVelocity = -1;
+		
+		angle = (float) Math.atan2(yVelocity, xVelocity);
 	}
 	
 	public void update() {		
-		//Constants velocity for now, implement acceleration or change of direction
+		//Constants velocity for now, implement acceleration or change of direction??
 		xPosition += xVelocity * Gdx.graphics.getDeltaTime();
 		yPosition += yVelocity * Gdx.graphics.getDeltaTime();
 	}
 	
 	public void render(SpriteBatch sb) {
+		sb.draw(beeTexture, xPosition - beeWidth / 2, yPosition - beeHeight / 2, xPosition, yPosition, beeWidth, beeHeight, 1.0f, 1.0f, angle);
 	}
 	
-	public void bounceOff() {
-		if (bounced == false) {
-			bounced = true;
-		}
+	public void bounceOff(float xUmbrella, float yUmbrella) { //Pass the position of the center of the arc of the umbrella to calculate collision
+		
+		bounced = true;
+		
+		//Change velocity according to collision physics
+		
+		double collisionAngle = Math.atan2(yPosition - yUmbrella, xPosition - xUmbrella);
 	}
 	
 	public float getX() {
