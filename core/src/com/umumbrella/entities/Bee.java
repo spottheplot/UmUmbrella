83f64590ed6129772dda60226e6c8c64d1c69e5a
Jabawaka <oldworldremains@gmail.com>
2014-12-07 02:02:58 +0000

    Bee class

--- core/src/com/umumbrella/entities/Bee.java
+++ core/src/com/umumbrella/entities/Bee.java
@@ -1,10 +1,9 @@
 package com.umumbrella.entities;
 
-import umumbrella.MainGame;
-import umumbrella.TextureManager;
+import com.umumbrella.game.MainGame;
+import com.umumbrella.framework.TextureManager;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.MathUtils;
@@ -34,12 +33,11 @@ public class Bee {
 			beeTexture = new TextureRegion(TextureManager.BOUNCEDBEE, beeWidth, beeHeight);
 		}		
 		
-		//Initialize position out of the screen over the top
-		
+		//Initialize position out of the screen over the top		
 		xPosition = MathUtils.random(0, MainGame.WIDTH);
 		yPosition = MathUtils.random(MainGame.HEIGHT, 2 * MainGame.HEIGHT);
 		
-		xVelocity = 0; //For now equal to 0, check for possible range in random
+		xVelocity = 0; //For now equal to 0 and -1, check for possible range in random
 		yVelocity = -1;
 		
 		angle = (float) Math.atan2(yVelocity, xVelocity);
@@ -52,28 +50,72 @@ public class Bee {
 	}
 	
 	public void render(SpriteBatch sb) {
-		sb.draw(beeTexture, xPosition - beeWidth / 2, yPosition - beeHeight / 2, xPosition, yPosition, beeWidth, beeHeight, 1.0f, 1.0f, angle);
+		sb.draw(beeTexture, xPosition - beeWidth / 2, yPosition - beeHeight / 2, xPosition, yPosition, beeWidth, beeHeight, 1.0f, 1.0f, angle * 180 / (float) Math.PI);
 	}
 	
-	public void bounceOff(float xUmbrella, float yUmbrella) { //Pass the position of the center of the arc of the umbrella to calculate collision
+	public void setxVelocity(float xVelocity) {
+		this.xVelocity = xVelocity;
+	}
+	
+	public void setyVelocity(float yVelocity) {
+		this.yVelocity = yVelocity;
+	}
+	
+	public void setxPosition(float xPosition) {
+		this.xPosition = xPosition;
+	}
+	
+	public void setyPosition(float yPosition) {
+		this.yPosition = yPosition;
+	}
+	
+	public void setBounced(boolean bounced) {
+		this.bounced = bounced;
+	}
+	
+	public float getxPosition() {
+		return xPosition;
+	}
+	
+	public float getyPosition() {
+		return yPosition;
+	}
+	
+	public float getxVelocity() {
+		return xVelocity;
+	}
+	
+	public float getyVelocity() {
+		return yVelocity;
+	}
+	
+	public boolean getBounced() {
+		return bounced;
+	}
+	
+	public void bounceOff(float xUmbrella, float yUmbrella) { //THIS METHOD NEEDS TO BE MOVED OUTSIDE. KEEP HERE FOR THE MATHS
 		
 		bounced = true;
 		
 		//Change velocity according to collision physics
 		
 		double collisionAngle = Math.atan2(yPosition - yUmbrella, xPosition - xUmbrella);
-	}
-	
-	public float getX() {
-		return xPosition;
-	}
-	
-	public float getY() {
-		return yPosition;
-	}
-	
-	public boolean getState() {
-		return bounced;
+		double rotationAngle = 2 * (Math.PI / 2 - collisionAngle);
+
+		double newXSpeed = 0;
+		double newYSpeed = 0;
+		
+		if (angle > collisionAngle + Math.PI) { 			//Rotate anti clock-wise
+			newXSpeed = Math.cos(rotationAngle) * xVelocity + Math.sin(rotationAngle) * yVelocity;
+			newYSpeed = -Math.sin(rotationAngle) * xVelocity + Math.cos(rotationAngle) * yVelocity;
+		}
+		else { 				//Rotate clock-wise
+			newXSpeed = Math.cos(rotationAngle) * xVelocity - Math.sin(rotationAngle) * yVelocity;
+			newYSpeed = Math.sin(rotationAngle) * xVelocity + Math.cos(rotationAngle) * yVelocity;
+		}
+		
+		xVelocity = (float) newXSpeed;
+		yVelocity = (float) newYSpeed;
 	}
 
 }
